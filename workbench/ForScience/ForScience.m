db700cddeb5c1f2bebe85bdce6de3cc425a5d9ed
Lukas Lang <langl@ethz.ch>
2018-03-11 14:06:40 +0100

    Fixed minor typos

---
+++ workbench/ForScience/ForScience.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench 11.03.2018 *)
+
+BeginPackage["ForScience`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
---
+++ workbench/PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2018.03.11*)
+
+Paclet[
+  Name -> "ForScience",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
