MMA-ForScience/ForScience

commit 7ebc4ec074f0c03132a75578f153fa3f376a4200

Reworked DocID to handle also type&label to unify link spec capabilities
Centralized link spec parsing to make supported formats uniform

ForScience/PacletInfo.m
@@ -4,7 +4,7 @@ Paclet[
   Name -> "ForScience",
   Description -> "Contains various utility functions and styling to make it easier to use MMA for scientific plots",
   Creator -> "Lukas Lang & Marc Lehner",
-  Version -> "0.67.3",
+  Version -> "0.67.4",
   MathematicaVersion -> "11.1+",
   Extensions -> {
     { "Documentation", Language -> "English"},

ForScience/PacletUtils/DocumentationCache.wl
@@ -62,9 +62,9 @@ DocumentationCacheGet[sym_,type_,OptionsPattern[]]:=With[
         Import[cacheFile<>".nb"]/.
          TagBox[
            _,
-           Hyperlink->{id_,tp_,linkFunc_:DocumentationLink}/;DocumentedQ[id,tp],
+           Hyperlink->{id_,linkFunc_}|id_/;DocumentedQ[id],
            OptionsPattern[]
-         ]:>linkFunc@spec
+         ]:>First[{linkFunc,DocumentationLink}]@id
       ]
     ];
     cacheFile<>".nb"

ForScience/PacletUtils/DocID.wl
@@ -18,21 +18,38 @@ HeldSymbol[sym_String?Internal`SymbolNameQ]:=ToExpression[sym,InputForm,Hold]
 Attributes[DocID]={HoldAll};
 
 
+$InvalidType=Context@$InvalidType<>"DocID`$InvalidType";
+
+
 DocID[id_DocID]:=id
-DocID[sym_String?DefinedQ]:=HeldSymbol[sym]/.Hold[s_]:>DocID[sym,s]
-DocID[tit_String]:=Select[
+DocID[spec_String,type_String:""]:=StringSplit[spec,{"::"->"::","//"},All]/.
+ {{tit_,"::",ref__}:>{tit,ref},{type2___,tit_}:>{tit,type2,tit}}/.
+  {tit_,type2_:"",name_}:>DocID[name,{type,type2},tit]
+DocID[name_String,types:{_String,_String},tit_String]:=DocID[name,Evaluate@Switch[types,
+  {"",_}|{_,""},
+  StringJoin@@types,
+  {t_,t_},
+  First@types,
+  _,
+  $InvalidType
+],tit]
+DocID[name_String?DefinedQ,type_String,tit_String]:=HeldSymbol[name]/.Hold[s_]:>DocID[name,s,type,tit]
+DocID[name_String,type_String,tit_String]:=Select[
   $DocumentedObjects,
   Function[sym,DocumentationTitle[sym,DocumentationType@sym]==tit,{HoldFirst}]
-]/.Hold[sym_:None,___]:>DocID[tit,sym]
-DocID[sym_Symbol]:=With[{type=DocumentationType[sym]},DocID[Evaluate@DocumentationTitle[sym,type],sym]/;type=!=None]
-DocID[sym_Symbol]:=DocID[Evaluate@SafeSymbolName@sym,sym]
+]/.Hold[sym_:None,___]:>DocID[name,sym,type,ref]
+DocID[sym_Symbol,type_:""]:=With[{rType=DocumentationType[sym,type]},DocID[Evaluate@DocumentationTitle[sym,rType],sym,rType]/;rType=!=None]
+DocID[sym_Symbol,type_:""]:=DocID[Evaluate@SafeSymbolName@sym,sym,type]
+DocID[name_String,sym_Symbol,type_String]:=DocID[name,sym,type,name]
 
 
-DocID[name_,_][SymbolName]:=name
-DocID[_,sym_][Symbol]:=Hold[sym]
+DocID[name_,_,_,_][SymbolName]:=name
+DocID[_,sym_,_,_][Symbol]:=Hold[sym]
+DocID[_,_,type_,_][DocumentationType]:=type
+DocID[_,_,_,tit_][Label]:=tit
 
 
-DocIDSpec=_String|_Symbol|_DocID;
+DocIDSpec=_DocID|PatternSequence[_String|_Symbol,type___String/;Length@{type}<=1];
 
 
 Attributes[DocumentationType]={HoldFirst};
@@ -47,28 +64,29 @@ DocumentationType[sym_Symbol,type_String:""]:=If[
   ],
   None
 ]
-DocumentationType[Evaluate[ref:DocIDSpec],type_String:""]:=With[
-  {heldSymbol=DocID[ref]@Symbol},
-  (heldSymbol/.Hold[s_]:>DocumentationType[s,type])/;heldSymbol=!=None
+DocumentationType[Evaluate[ref:DocIDSpec]]:=With[
+  {id=DocID[ref]},
+  (id@Symbol/.Hold[s_]:>DocumentationType[s,id@DocumentationType])/;id@Symbol=!=None
 ]
-DocumentationType[Evaluate@DocIDSpec,_String:""]:=None
+DocumentationType[Evaluate@DocIDSpec]:=None
 
 
 Attributes[DocSearch]={HoldFirst};
 
 
-DocSearch[Evaluate[ref:DocIDSpec],type_String:""]:=DocSearch[ref,type]=With[
+Clear@DocSearch;
+DocSearch[Evaluate[ref:DocIDSpec]]:=DocSearch[ref]=With[
   {id=DocID@ref},
   "Matches"/.
   SearchDocumentation[
-    StringTemplate["+(ExactTitle:\"``\") +(NotebookType:``)"][id@SymbolName,type],
+    StringTemplate["+(ExactTitle:\"``\") +(NotebookType:``)"][id@SymbolName,id@DocumentationType],
     "Limit"->1,
     "MetaData"->{"Type","URI"}
   ]/.{
-    {}->{type,Missing[]},
+    {}->{id@DocumentationType,Missing[]},
     {{tp_,uri_}}:>{tp,"paclet:"<>uri}
   }/.{
-    {"Format",uri_}:>{DocID["\""<>id@SymbolName<>"\""],uri},
+    {"Format",uri_}/;id@SymbolName==id@Label:>{DocID["\""<>id@SymbolName<>"\""],uri},
     {_,uri_}:>{id,uri}
   }
 ]
@@ -95,9 +113,9 @@ Attributes[DocumentationPath]={HoldFirst};
 Options[DocumentationPath]={"IncludeContext"->True};
 
 
-DocumentationPath[Evaluate[ref:DocIDSpec],type_String:"",OptionsPattern[]]:=With[
+DocumentationPath[Evaluate[ref:DocIDSpec],OptionsPattern[]]:=With[
   {id=DocID@ref},
-  DocumentationType[id,type]/.{
+  DocumentationType[id]/.{
     rType:Except[None]:>StringTemplate["``ReferencePages/``/``"][
       If[OptionValue["IncludeContext"],
         First@StringSplit[Context@@id[Symbol],"`"]<>"/",
@@ -113,15 +131,15 @@ DocumentationPath[Evaluate[ref:DocIDSpec],type_String:"",OptionsPattern[]]:=With
 Attributes[RawDocumentationLink]={HoldFirst};
 
 
-RawDocumentationLink[Evaluate[ref:DocIDSpec],type_String:""]:=With[
+RawDocumentationLink[Evaluate[ref:DocIDSpec]]:=With[
   {id=DocID@ref},
   With[
-    {res=DocSearch[id,type]},
+    {res=DocSearch[id]},
     If[
       !MissingQ@Last@res,
       res,
-      DocumentationPath[id,type]/.{
-        None:>(Sow[{id,type},Hyperlink];{First@res,Missing[]}),
+      DocumentationPath[id]/.{
+        None:>(Sow[id,Hyperlink];{First@res,Missing[]}),
         path_:>{First@res,"paclet:"<>path}
       }
     ]
@@ -135,11 +153,11 @@ Attributes[DocumentationLink]={HoldFirst};
 Options[DocumentationLink]={"LinkStyle"->"RefLink",BaseStyle->{"InlineFormula"}};
 
 
-DocumentationLink[Evaluate[ref:DocIDSpec],type_String:"",OptionsPattern[]]:=With[
+DocumentationLink[Evaluate[ref:DocIDSpec],OptionsPattern[]]:=With[
   {id=DocID@ref},
-  RawDocumentationLink[id,type]/.{
-    {tit_,_Missing}->TagBox[tit@SymbolName,Hyperlink->{id,type},BaseStyle->OptionValue[BaseStyle]],
-    {tit_,uri_}->TemplateBox[{tit@SymbolName,uri},OptionValue["LinkStyle"],BaseStyle->OptionValue[BaseStyle]]
+  RawDocumentationLink[id]/.{
+    {rId_,_Missing}->TagBox[rId@Label,Hyperlink->id,BaseStyle->OptionValue[BaseStyle]],
+    {rId_,uri_}->TemplateBox[{rId@Label,uri},OptionValue["LinkStyle"],BaseStyle->OptionValue[BaseStyle]]
   }
 ]
 
@@ -147,7 +165,7 @@ DocumentationLink[Evaluate[ref:DocIDSpec],type_String:"",OptionsPattern[]]:=With
 Attributes[DocumentedQ]={HoldFirst};
 
 
-DocumentedQ[Evaluate[ref:DocIDSpec],type_String:""]:=!MissingQ@Last@RawDocumentationLink[ref,type]
+DocumentedQ[Evaluate[ref:DocIDSpec]]:=!MissingQ@Last@RawDocumentationLink[ref]
 
 
 End[]

ForScience/PacletUtils/DocUtils.wl
@@ -31,8 +31,7 @@ BoxesToDocEntry[boxes:(_RowBox|_TagBox)]:=Replace[
       {    
         TagBox[RowBox@l_List,"[**]"]:>
          RowBox@Replace[l,s_String/;DefinedQ@s:>DocumentationLink[s,"Symbol"],1],
-        TagBox[RowBox@l:{__String},"<**>"]:>
-         DocumentationLink@@Reverse@StringSplit[StringJoin@l,"/",2]
+        TagBox[RowBox@l:{__String},"<**>"]:>DocumentationLink@StringJoin@l
       },
       All
     ],

ForScience/PacletUtils/DocumentationHeader.wl
@@ -93,18 +93,18 @@ With[
 ]
 
 
-HeaderDropdownLink/:(ref_:>HeaderDropdownLink[ref_,type_]):=RawDocumentationLink[ref,type]/.{
-  {tit_,_Missing}->(tit@SymbolName:>TagBox[ref,Hyperlink->{ref,type,HeaderDropdownLink}]),
-  {tit_,uri_}->(tit@SymbolName:>Documentation`HelpLookup[uri])
+HeaderDropdownLink/:(ref_:>HeaderDropdownLink[ref_]):=RawDocumentationLink[ref]/.{
+  {id_,_Missing}->(id@Label:>TagBox[ref,Hyperlink->{id,HeaderDropdownLink}]),
+  {id_,uri_}->(id@Label:>Documentation`HelpLookup[uri])
 }
 
 
-MakeHeaderDropdown[title_,style_,refs_,pageType_]:=If[Length@refs>0,
+MakeHeaderDropdown[title_,style_,refs_]:=If[Length@refs>0,
   Cell[
     BoxData@TagBox[
       ActionMenuBox[
         FrameBox[Cell[TextData[{title," ",$HeaderMenuArrow}]],StripOnInput->False],
-        #:>HeaderDropdownLink[#,pageType]&/@refs,
+        #:>HeaderDropdownLink[#]&/@refs,
         Appearance->None,
         MenuAppearance->Automatic,
         MenuStyle->style

ForScience/PacletUtils/FormatUsageDoc.wl
@@ -23,7 +23,8 @@ Details[FormatUsage]={
   "[*FormatUsage*] effectively calls [*FormatUsageCase*] with [*StartOfLine->True*].",
   "Format specifications can be arbitrarily nested.",
   "The formatted strings returned by [*FormatUsage*] are usable as usage messages for symbols.",
-  BoxData@RowBox@{"In ",StyleBox["<*\!\(\*StyleBox[\"spec\", \"TI\"]\)*>","CodeFont"],FormatUsage@", ```spec``` either be the exact title of a documentation page or ```type```/```title```, where type is any documentation page type, such as Symbol,Format,Workflow,..."}
+  BoxData@RowBox@{"In ",StyleBox["<*\!\(\*StyleBox[\"spec\", \"TI\"]\)*>","CodeFont"],FormatUsage@", ```spec``` can either be the exact title of a documentation page or ```type```//```title```, where type is any documentation page type, such as Symbol,Format,Workflow,..."},
+  BoxData@RowBox@{"The label of a link specified by ",StyleBox["<*\!\(\*StyleBox[\"spec\", \"TI\"]\)*>","CodeFont"],FormatUsage@" can be specified by prepending ```label```::, i.e. ```label```::```title``` or ```label```::```type```//```title```."}
 };
 
 

ForScience/PacletUtils/Guides.wl
@@ -39,12 +39,14 @@ MakeGuidesHeader[sym_]:=MakeHeaderDropdown[
   "MoreAbout",
   Replace[
     Guides[sym],
-    s_Symbol?(Not@*GuideQ):>(
-      Message[Guides::invalidSymbol,HoldForm@s];Nothing
-    ),
+    {
+      s_Symbol?(Not@*GuideQ):>(
+        Message[Guides::invalidSymbol,HoldForm@s];Nothing
+      ),
+      g_:>DocID[g,"Guide"]
+    },
     1
-  ],
-  "Guide"
+  ]
 ]
 
 

ForScience/PacletUtils/SeeAlso.wl
@@ -24,7 +24,7 @@ MakeSeeAlsoSection[sym_,nb_,OptionsPattern[]]:=If[Length@SeeAlso@sym>0,
       Cell["See Also","SeeAlsoSection"],
       Cell[
         TextData@Riffle[
-          CodeCell@DocumentationLink[#,"Symbol"]&/@List@@DocID/@SeeAlso[sym],
+          CodeCell@*DocumentationLink/@List@@Replace[SeeAlso[sym],s_:>DocID[s,"Symbol"],1],
           Unevaluated@Sequence["\[NonBreakingSpace]",StyleBox["\[MediumSpace]\[FilledVerySmallSquare]\[MediumSpace]","InlineSeparator"]," "]
         ],
         "SeeAlso"
@@ -37,7 +37,7 @@ MakeSeeAlsoSection[sym_,nb_,OptionsPattern[]]:=If[Length@SeeAlso@sym>0,
 Attributes[MakeSeeAlsoHeader]={HoldFirst};
 
 
-MakeSeeAlsoHeader[sym_]:=MakeHeaderDropdown["See Also","SeeAlso",List@@DocID/@SeeAlso[sym],"Symbol"]
+MakeSeeAlsoHeader[sym_]:=MakeHeaderDropdown["See Also","SeeAlso",List@@Replace[SeeAlso[sym],s_:>DocID[s,"Symbol"],1]]
 
 
 AppendTo[$DocumentationSections["Symbol"],MakeSeeAlsoSection];

commit d25d6d01733348e0f37ca82c1962b70197fc3e14

Fixed DocumentationCacheGet never updating Hyperlinks
Probably caused by 7ebc4ec, which merged symbol type into DocID

ForScience/PacletInfo.m
@@ -4,7 +4,7 @@ Paclet[
   Name -> "ForScience",
   Description -> "Contains various utility functions and styling to make it easier to use MMA for scientific plots",
   Creator -> "Lukas Lang & Marc Lehner",
-  Version -> "0.87.54",
+  Version -> "0.87.55",
   MathematicaVersion -> "11.1+",
   Extensions -> {
     { "Documentation", Language -> "English"},

ForScience/PacletUtils/DocumentationCache.wl
@@ -75,7 +75,7 @@ DocumentationCacheGet[sym_,type_,OptionsPattern[]]:=With[
       cacheData=Import[cacheFile<>".mx"]
     },
     If[cacheData["Dependencies"]=!=curID,Return@Null];
-    If[AnyTrue[cacheData[Hyperlink],Apply[DocumentedQ]],
+    If[AnyTrue[cacheData[Hyperlink],DocumentedQ],
       Export[
         cacheFile<>".nb",
         Import[cacheFile<>".nb"]/.